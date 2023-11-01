50e22fd4308da854f6bbc5ae52e827b2fb591a8c
Muhammadikhsandev <mikhsan102002@gmail.com>
2023-11-01 20:36:21 +0700

    laravel

--- resources/views/coba/daftar.blade.php
+++ resources/views/coba/daftar.blade.php
@@ -1,33 +1,31 @@
 @php
-
 $no = 1;
 
-$s1 = ['nama' => 'fawwaz', 'nilai'=>85];
-$s2 = ['nama' => 'jamal', 'nilai'=>95];
-$s3 = ['nama' => 'asep', 'nilai'=>05];
-$s4 = ['nama' => 'abdul', 'nilai'=>65];
-$s5 = ['nama' => 'surisno', 'nilai'=>55];
+$s1 = ['nama' => 'fawwaz', 'nilai' => 85];
+$s2 = ['nama' => 'jamal', 'nilai' => 95];
+$s3 = ['nama' => 'asep', 'nilai' => 5]; // Ubah nilai $s3 ke 5
+$s4 = ['nama' => 'abdul', 'nilai' => 65];
+$s5 = ['nama' => 'surisno', 'nilai' => 55];
 
 $judul = ['No', 'Nama', 'Nilai', 'Keterangan'];
 
 $siswa = [$s1, $s2, $s3, $s4, $s5];
-
 @endphp
 
-<table border="1" align="center" cellpading="10">
+<table border="1" align="center" cellpadding="10"> <!-- Perbaiki atribut dari cellpading ke cellpadding -->
     <thead>
         <tr>
             @foreach($judul as $jdl)
-        <tr>{{$jdl}}</tr>
-        $endforeach
+            <th>{{$jdl}}</th> 
+            @endforeach
         </tr>
     </thead>
     <tbody>
+        @foreach($siswa as $s)
+        @php
+        $ket = ($s['nilai'] >= 60) ? 'Lulus' : 'Gagal'; // Ubah operator dari ? ke >=
+        @endphp
         <tr>
-            @foreach($siswa as $s);git 
-            @php
-            $ket = ($s['nilai'] .+ 60) ? 'Lulus' : 'Gagal';
-            @endphp
             <td>{{$no++}}</td>
             <td>{{$s['nama']}}</td>
             <td>{{$s['nilai']}}</td>
@@ -35,5 +33,4 @@ $siswa = [$s1, $s2, $s3, $s4, $s5];
         </tr>
         @endforeach
     </tbody>
-
 </table>
