d2f4a5ce0babb32cbdc9c96df4a773b41d0d3f25
Muhammadikhsandev <mikhsan102002@gmail.com>
2023-11-09 12:09:05 +0700

    mengedit dan menghapus data pada laravel

--- resources/views/admin/produk/index.blade.php
+++ resources/views/admin/produk/index.blade.php
@@ -25,6 +25,8 @@
                                             <th>Stok</th>
                                             <th>Minimal Stok</th>
                                             <th>Jenis Produk</th>
+                                            <th>Action</th>
+
                                             
                                         </tr>
                                     </thead>
@@ -38,6 +40,8 @@
                                             <th>Stok</th>
                                             <th>Minimal Stok</th>
                                             <th>Jenis Produk</th>
+                                            <th>Action</th>
+
                                         </tr>
                                     </tfoot>
                                     <tbody>
@@ -53,6 +57,34 @@
                                             <td>{{$pr->stok}}</td>
                                             <td>{{$pr->min_stok}}</td>
                                             <td>{{$pr->jenis}}</td>
+                                            <td>
+                                                <a href="{{url('admin/produk/show/'.$pr->id)}}" class="btn btn-sm btn-info"><i class="fas fa-eye"></i></a>
+                                                <a href="{{url('admin/produk/edit/'.$pr->id)}}" class="btn btn-sm btn-warning"><i class="fas fa-edit"></i></a>
+                                                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#exampleModal{{$pr->id}}">
+<i class="fas fa-trash"></i>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal{{$pr->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Apakah anda ingin menghapusnya</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ya, saya ingin menghapusnya {{$pr->nama}} ?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+       <a href="{{url('admin/produk/delete/'.$pr->id)}}" type="button" class="btn btn-danger">Delete</a> 
+      </div>
+    </div>
+  </div>
+</div>
+                                            </td>
                                            
                                         </tr>
                                         
