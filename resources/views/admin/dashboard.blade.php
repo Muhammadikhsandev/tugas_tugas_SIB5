98e1abeb7db806035e8e569366febf9787ed3066
Muhammadikhsandev <mikhsan102002@gmail.com>
2023-11-15 08:35:51 +0700

    char

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -105,22 +105,6 @@
                                     Char data dari produk dengan total produk {{$produk}}
                                 </div>
                             </div>
-
-                            <!-- Bar Chart -->
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Bar Chart</h6>
-                                </div>
-                                <div class="card-body">
-                                    <div class="chart-bar">
-                                        <canvas id="myBarChart"></canvas>
-                                    </div>
-                                    <hr>
-                                    Styling for the bar chart can be found in the
-                                    <code>/js/demo/chart-bar-demo.js</code> file.
-                                </div>
-                            </div>
-
                         </div>
 
                         <!-- Donut Chart -->
