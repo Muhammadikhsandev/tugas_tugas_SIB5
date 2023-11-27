f97acb5810d5eb9e7bed8a6f51c65f7602e4b2df
Muhammadikhsandev <mikhsan102002@gmail.com>
2023-11-27 21:28:16 +0700

    otentikasi otorisasi middleware

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -18,7 +18,7 @@
                     <div class="h5 mb-0 font-weight-bold text-gray-800">{{$produk}}</div>
                 </div>
                 <div class="col-auto">
-                   <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                  <i class="fas fa-calendar fa-2x text-gray-300"></i>
                     <!-- <i class="fab fa-product-hunt"></i> -->
                 </div>
             </div>
